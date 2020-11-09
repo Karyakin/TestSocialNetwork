ef848482f5a1023a83aa46c7a5d9b3513808ac61
Дмитрий Карякин <375447955552dk@gmail.com>
2020-11-09 19:11:35 +0300

    Создание нового контрола + возврат различных кодов

---
+++ API/Controllers/CarsController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]//это атрибут говорит о том, по какому роуту вызывается контроллер и мы ему говорим, //что это роут вызывается по имени контроллера. Т.Е зайдем в этот контроллер по имени Users 
+    public class CarsController : ControllerBase
+    {
+        private readonly DataContext _dataContext;
+        public CarsController(DataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+
+        public ActionResult GetRez()
+        {
+
+            return Ok("Return OK");
+        }
+
+
+        [HttpGet("U1")]
+        public IActionResult GetRex()
+        {
+            return Ok("Return OK by Interface");
+        }
+
+        [HttpGet("U2")]
+        public IActionResult GetStatusCode()
+        {
+            return StatusCode(401);
+        }
+
+        [HttpGet("U3")]
+        public IActionResult GetContent()
+        {
+            return Content("GetContent");
+        }
+
+        [HttpGet("U4")]
+        public IActionResult NotFoundResult()
+        {
+            return NotFound("NotFound");
+        }
+
+        [HttpGet("U5")]
+        public IActionResult UnauthorizedResult(int a)
+        {
+            if (a < 18)
+                return Unauthorized();
+            return Content("All Right");
+
+
+        }
+    }
+}
