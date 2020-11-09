bf1c874d7f1a3ed5d286c91c1f23a520a654c1a7
Дмитрий Карякин <375447955552dk@gmail.com>
2020-11-09 16:13:45 +0300

    Внедрили датаконтекст

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -2,15 +2,19 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using API.Data;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
 {
-    public class UsersController : Controller
+    [ApiController]
+    [Route("[controller]")]
+    public class UsersController : ControllerBase//Базовый класс без поддержки View
     {
-        public IActionResult Index()
+        public DataContext _dataContext { get; set; }
+        public UsersController(DataContext dataContext)
         {
-            return View();
+            _dataContext = dataContext;
         }
     }
 }
