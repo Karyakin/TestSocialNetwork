f89cf392404c0b08446c5b0744f28603238e6367
Дмитрий Карякин <375447955552dk@gmail.com>
2020-11-09 17:51:34 +0300

    тренил [HttpGet("U1")]

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -8,13 +8,28 @@ using Microsoft.AspNetCore.Mvc;
 namespace API.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("[controller]")]//это атрибут говорит о том, по какому роуту вызывается контроллер и мы ему говорим, 
+                            //что это роут вызывается по имени контроллера. Т.Е зайдем в этот контроллер по имени Users 
     public class UsersController : ControllerBase//Базовый класс без поддержки View
     {
         public DataContext _dataContext { get; set; }
         public UsersController(DataContext dataContext)
         {
             _dataContext = dataContext;
+        }
+
+       [HttpGet("U1")]
+        public ActionResult GetUsers(string a)//  Тип ActionResult позволяет возвращать из метода коды с котороми отрабатывает метод
+        {
+            return BadRequest(a);
+        }
+
+
+        [HttpGet("U2")]
+        public ActionResult GetUsers(int a)//  Тип ActionResult позволяет возвращать из метода коды с котороми отрабатывает метод
+        {
+          
+            return BadRequest("Пришел тип инт");
         }
     }
 }
